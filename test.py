738576601df7298977dd73401e389c4875f37907
tourist-hillel <qinfaustmager@gmail.com>
2026-06-16 20:00:40 +0300

    fix: rebase 'main'

---
+++ test.py
@@ -0,0 +1,12 @@
+# def calc(x: int, y: int) -> int:
+#     return x.__mul__(y) # x*y -> x.__mul__(y)
+
+
+# print(calc(3.0, 5))
+
+a = '2'
+
+if hasattr(a, '__mul__') and isinstance(a, int):
+    print(a*a)
+else:
+    print(f'"{a}" is a string and we cant do multiply')
